8ce52ba99af3501a6c70f7c81a5fc0e8f6387a02
TomWan <wanmingtom@gmail.com>
2017-11-01 21:06:40 +0800

    feat: add default family support

---
+++ test/default_family.js
@@ -0,0 +1,98 @@
+const _ = require('lodash')
+const test = require('ava')
+const uuid = require('uuid/v4')
+const helper = require('./helper')
+
+const hb = helper.createHb({
+  defaultFamily: 'cf1'
+})
+
+test('Get data correctly', async t => {
+  const rowKey = uuid()
+  const stringValue = uuid()
+  const dateValue = new Date()
+
+  const value = {
+    'string': stringValue,
+    'cf2:date': dateValue
+  }
+
+  await hb.Test.put(rowKey, value)
+
+  const ret = await hb.Test.get(rowKey)
+
+  t.true(_.isDate(ret['cf2:date']))
+  t.deepEqual(ret, {
+    'string': stringValue,
+    'cf2:date': dateValue,
+    'cf2:buffer': null,
+    'cf2:number': null
+  })
+})
+
+test('Get data correctly with full col name', async t => {
+  const rowKey = uuid()
+  const stringValue = uuid()
+  const dateValue = new Date()
+
+  const value = {
+    'cf1:string': stringValue,
+    'cf2:date': dateValue
+  }
+
+  await hb.Test.put(rowKey, value)
+
+  const ret = await hb.Test.get(rowKey)
+
+  t.true(_.isDate(ret['cf2:date']))
+  t.deepEqual(ret, {
+    'string': stringValue,
+    'cf2:date': dateValue,
+    'cf2:buffer': null,
+    'cf2:number': null
+  })
+})
+
+test('Replace a non-null value with null successfully', async t => {
+  const rowKey = uuid()
+  const stringValue = uuid()
+
+  await hb.Test.put(rowKey, {
+    'string': stringValue,
+    'cf2:number': 123
+  })
+
+  await hb.Test.put(rowKey, {
+    'string': null
+  })
+
+  const ret = await hb.Test.get(rowKey)
+  t.deepEqual(ret, {
+    'string': null,
+    'cf2:date': null,
+    'cf2:buffer': null,
+    'cf2:number': 123
+  })
+})
+
+test('Replace a non-null value with null successfully with full col name', async t => {
+  const rowKey = uuid()
+  const stringValue = uuid()
+
+  await hb.Test.put(rowKey, {
+    'cf1:string': stringValue,
+    'cf2:number': 123
+  })
+
+  await hb.Test.put(rowKey, {
+    'cf1:string': null
+  })
+
+  const ret = await hb.Test.get(rowKey)
+  t.deepEqual(ret, {
+    'string': null,
+    'cf2:date': null,
+    'cf2:buffer': null,
+    'cf2:number': 123
+  })
+})
